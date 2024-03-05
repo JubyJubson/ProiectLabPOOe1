5f1ab2b8387fae8c5d13df16cfbae20024233ff0
Ionnier <dbahrim@bitdefender.com>
2024-03-05 13:51:18 +0200

    Add unit tests

---
+++ test/example/example_test.cpp
@@ -0,0 +1,7 @@
+#include <gtest/gtest.h>
+
+TEST(SuiteName, TestName) {
+    int x = 10;
+    EXPECT_EQ(x, 10);
+    // Other assertions: https://google.github.io/googletest/reference/assertions.html#boolean
+}
